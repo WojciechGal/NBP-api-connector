ddd13ffb383491cf4508151e51ee8eee0855581b
Wojciech <wojciech.gladalski@gmail.com>
2019-12-21 16:23:15 +0100

    list extraction

--- src/main/java/pl/wojciech/nbpapi/exchange/NbpService.java
+++ src/main/java/pl/wojciech/nbpapi/exchange/NbpService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -14,7 +16,7 @@ public class NbpService implements ExchangeRate {
     private static final String NBP_API_TABLE = "http://api.nbp.pl/api/exchangerates/tables/A?format=json";
 
     @Override
-    public void getDataFromTable() {
+    public List<RateDto> getDataFromTable() {
 
         RestTemplate restTemplate = new RestTemplate();
 
@@ -25,5 +27,11 @@ public class NbpService implements ExchangeRate {
         Arrays.stream(body).map(TableDto::getRates)
                 .flatMap(b -> b.stream())
                 .forEach(r -> log.info("rate: {}", r));
+
+        List<RateDto> rates = Arrays.stream(body).map(TableDto::getRates)
+                .flatMap(b -> b.stream())
+                .collect(Collectors.toList());
+
+        return rates;
     }
 }
