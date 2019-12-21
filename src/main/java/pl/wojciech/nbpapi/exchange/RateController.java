f56a2f135530aafce5fe3cbaad613b21211d23fa
Wojciech <wojciech.gladalski@gmail.com>
2019-12-21 16:24:49 +0100

    givin json response to browser

--- src/main/java/pl/wojciech/nbpapi/exchange/RateController.java
+++ src/main/java/pl/wojciech/nbpapi/exchange/RateController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 @AllArgsConstructor
 public class RateController {
@@ -13,8 +15,7 @@ public class RateController {
 
     @RequestMapping("/")
     @ResponseBody
-    public String getConcurencyTable() {
-        exchangeRate.getDataFromTable();
-        return "--- ---";
+    public List<RateDto> getConcurencyTable() {
+        return exchangeRate.getDataFromTable();
     }
 }
